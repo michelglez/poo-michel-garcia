9a93f253d7b1b364794148a0215356e344add8bd
michelglez <al03001350@tecmilenio.mx>
2025-01-14 18:16:07 -0600

    commit1

---
+++ poo/src/main/java/edu/michelle/garcia/actividades/actividad1/Starter.java
@@ -0,0 +1,9 @@
+package edu.michelle.garcia.actividades.actividad1;
+
+import edu.michelle.garcia.actividades.actividad1.ui.CLI;
+
+public class Starter {
+    public static void main(String[] args) {
+        CLI.ShowMenu();
+    }
+}
---
+++ poo/src/main/java/edu/michelle/garcia/actividades/actividad1/ui/CLI.java
@@ -0,0 +1,10 @@
+package edu.michelle.garcia.actividades.actividad1.ui;
+
+public class CLI {
+    public static void ShowMenu() {
+        System.out.println("app");
+        System.out.println("1. carro");
+        System.out.println("2. km"); 
+    }
+
+}

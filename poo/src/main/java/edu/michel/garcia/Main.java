172889ea32dcd6682b1b43f1e4126169fd586fec
michelglez <al03001350@tecmilenio.mx>
2025-01-17 19:38:52 -0600

    video_prueba

--- poo/src/main/java/edu/michel/garcia/Main.java
+++ poo/src/main/java/edu/michel/garcia/Main.java
@@ -1,4 +1,4 @@
-package edu.michelle.garcia;
+package edu.michel.garcia;
 
 public class Main {
     public static void main(String[] args) {
--- poo/src/main/java/edu/michelle/garcia/actividades/actividad1/Starter.java
+++
@@ -1,9 +0,0 @@
-package edu.michelle.garcia.actividades.actividad1;
-
-import edu.michelle.garcia.actividades.actividad1.ui.CLI;
-
-public class Starter {
-    public static void main(String[] args) {
-        CLI.ShowMenu();
-    }
-}

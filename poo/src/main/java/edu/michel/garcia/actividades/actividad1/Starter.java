0872dea19253dad53589130b78efb92bd1af865e
michelglez <al03001350@tecmilenio.mx>
2025-01-18 22:48:55 -0600

    Documentacion

--- poo/src/main/java/edu/michel/garcia/actividades/actividad1/Starter.java
+++ poo/src/main/java/edu/michel/garcia/actividades/actividad1/Starter.java
@@ -4,6 +4,6 @@ import edu.michel.garcia.actividades.actividad1.ui.CLI;
 
 public class Starter {
     public static void main(String[] args) {
-        CLI.mostrarMenu();
+        CLI.runApp();
     }
 }
--- poo/src/main/java/edu/michel/garcia/actividades/actividad1/ui/CLI.java
+++ poo/src/main/java/edu/michel/garcia/actividades/actividad1/ui/CLI.java
@@ -1,24 +1,65 @@
 package edu.michel.garcia.actividades.actividad1.ui;
 
+import java.util.Scanner;
+
+import edu.michel.garcia.actividades.actividad1.models.Car;
+import edu.michel.garcia.actividades.actividad1.process.StockManager;
+
+/**
+ * @param scanner Registra la informacion del carro
+ */
+
 public class CLI {
-    public static void mostrarMenu() {
-        System.out.println("1. Crear un nuevo estudiante");
-        System.out.println("2. Lista estudiantes");
-        System.out.println("3. Salir"); 
+    public static void runApp() {
+        Scanner scanner = new Scanner(System.in);
+        StockManager stockmanager = new StockManager();
+        showMenu();
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
+
+        while (opcion !=3){
+            switch (opcion) {
+                case 1:
+                    System.out.println("Introduce el numero de ventanas");
+                    int numVentanas = scanner.nextInt(); scanner.nextLine(); 
+                    System.out.println("Introduce el modelo");
+                    String modelo =scanner.nextLine();
+                    System.out.println("Introduce el color");
+                    String color =scanner.nextLine();
+                    System.out.println("Introduce la marca");
+                    String marca =scanner.nextLine();
+                    stockmanager.addCar(modelo,marca,numVentanas,color);
+                    break;
+                case 2:
+                   int auto=1;
+                    for(Car car : stockmanager.getStock()){
+                        System.out.println("Auto"+auto+":");
+                        System.out.println("    Modelo: \n" +car.getModelo());
+                        System.out.println("    Color: \n" +car.getColor ());
+                        System.out.println("    Numero de Ventanas: \n" +car.getNumeroVentanas());
+                        System.out.println("    Marca: \n" +car.getMarca ());
+                        auto++;
+                    } 
+                    break;
+                default:
+                    System.out.println("Opción no valida");
+                    
+            }
+            showMenu();
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+        }
+
     }
 
-    public void mostrarMensaje(String mensaje) {
-        System.out.println(mensaje);
+
+    public static void showMenu(){
+        System.out.println("Menu");
+        System.out.println("1. Agregar Carro al catalogo");
+        System.out.println("2. Mostrar catalogo");
+        System.out.println("3. Salir");
     }
 
-    public void mostrarError(String mensaje) {
-        System.out.println("Error: "+ mensaje);
-    }
-
-    public void mostrarEstudiantes(String[] estudiantes) {
-        for (String estudiante : estudiantes)
-        System.out.println(estudiante);
-    }
-
-
-}
+}   
+            
+ 
